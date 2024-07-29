709c6e66d04ff7ed9d70b00b71728e01e063c962
nishaaanth2 <itismenishaanth@gmail.com>
2024-07-30 05:03:16 +0530

    bg change

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -2,7 +2,7 @@
 
 export default function AuthLayout({ children }) {
   return (
-    <main className={` w-full h-screen flex justify-center items-center`}>
+    <main className={` w-full h-screen bg-[url('/irctc.png')] bg-no-repeat bg-cover bg-right-top scale flex justify-center items-center`}>
 
       {children}
     
