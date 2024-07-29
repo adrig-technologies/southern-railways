dd33ea8917bcaf3e9082fc04a42d36d33b6ea5ce
vijayasvj <vj.as.entrepreneur@gmail.com>
2024-07-29 15:46:43 +0530

    backend alone

---
+++ backend/test.py
@@ -0,0 +1,16 @@
+import requests
+
+# The URL of the Flask server
+url = 'http://127.0.0.1:5000/upload'
+
+# Path to the CSV file you want to upload
+csv_file_path = '/Users/apple/Desktop/Request Table - Untitled spreadsheet - Request.csv'
+
+# Open the CSV file in binary mode and send the request
+with open(csv_file_path, 'rb') as file:
+    files = {'file': file}
+    response = requests.post(url, files=files)
+
+# Print the response from the server
+print(response.status_code)
+print(response.json())
