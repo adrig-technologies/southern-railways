dc129c45aa39a8bb26f2322dae8d1edab2d50cc2
Vijay Murugadoss <65366176+vijayasvj@users.noreply.github.com>
2024-08-07 15:46:43 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -194,13 +194,280 @@ def check_table():
     table_has_rows = row_count > 0
     return jsonify({'has_rows': table_has_rows})
 
-@app.route('/optimize', methods=['GET'])
-def optimize_table():
+# List of block_section_yards
+BLOCK_SECTION_YARDS = [
+    "AJJ-AJJN", "MLPM-AJJN", "AJJN-TRT", "TRT-POI", "POI-VKZ", "VKZ-NG", 
+    "NG-EKM", "EKM-VGA", "VGA-PUT", "PUT-TDK", "TDK-PUDI", "PUDI-RU"
+]
+
+def fetch_requests_from_db():
     requests = RequestTable.query.all()
-    request_list = []
-    for req in requests:
-        request_data = {
-            'id': req.id,
+    return [{
+        'block_section_yard': request.block_section_yard,
+        'date': request.date,
+        'start_time': request.demanded_time_from,
+        'end_time': request.demanded_time_to,
+        'dept': request.dept
+    } for request in requests]
+
+def parse_time(time_str):
+    return datetime.strptime(time_str, "%H:%M")
+
+def merge_intervals(intervals):
+    sorted_intervals = sorted(intervals, key=lambda x: x['start'])
+    merged = []
+    for interval in sorted_intervals:
+        if not merged or merged[-1]['end'] < interval['start']:
+            merged.append(interval)
+        else:
+            merged[-1]['end'] = max(merged[-1]['end'], interval['end'])
+    return merged
+
+def find_shadow_blocks(allocated_requests):
+    shadow_blocks = {}
+
+    for request in allocated_requests:
+        block_section_yard_index = BLOCK_SECTION_YARDS.index(request['block_section_yard'])
+        start_time = parse_time(request['start_time'])
+        end_time = parse_time(request['end_time'])
+
+        for i in range(len(BLOCK_SECTION_YARDS)):
+            block_section_yard = BLOCK_SECTION_YARDS[i]
+            if block_section_yard not in shadow_blocks:
+                shadow_blocks[block_section_yard] = {}
+            if request['date'] not in shadow_blocks[block_section_yard]:
+                shadow_blocks[block_section_yard][request['date']] = []
+
+            if i == block_section_yard_index:
+                shadow_blocks[block_section_yard][request['date']].append({'start': start_time, 'end': end_time})
+            else:
+                if block_section_yard_index < i:
+                    shadow_blocks[block_section_yard][request['date']].append({'start': start_time, 'end': end_time})
+                else:
+                    overlap_intervals = [interval for interval in shadow_blocks[block_section_yard][request['date']] if not (interval['end'] <= start_time or interval['start'] >= end_time)]
+                    if not overlap_intervals:
+                        shadow_blocks[block_section_yard][request['date']].append({'start': start_time, 'end': end_time})
+
+    for block_section_yard in shadow_blocks:
+        for date in shadow_blocks[block_section_yard]:
+            shadow_blocks[block_section_yard][date] = merge_intervals(shadow_blocks[block_section_yard][date])
+
+    return shadow_blocks
+
+def allocate_non_engg_requests(non_engg_requests, shadow_blocks, sorted_dates):
+    allocated_non_engg_requests = []
+
+    for request in non_engg_requests:
+        for date in sorted_dates:
+            if date >= request['date']:
+                if request['block_section_yard'] in shadow_blocks and date in shadow_blocks[request['block_section_yard']]:
+                    for block in shadow_blocks[request['block_section_yard']][date]:
+                        block_duration = (block['end'] - block['start']).seconds / 3600
+                        requested_duration = (parse_time(request['end_time']) - parse_time(request['start_time'])).seconds / 3600
+                        
+                        if requested_duration <= block_duration:
+                            allocated_non_engg_requests.append({
+                                'block_section_yard': request['block_section_yard'],
+                                'date': date,
+                                'start_time': block['start'].strftime("%H:%M"),
+                                'end_time': (block['start'] + timedelta(hours=requested_duration)).strftime("%H:%M"),
+                                'dept': request['dept']
+                            })
+                            # Update shadow block to reflect allocated request
+                            block['start'] = block['start'] + timedelta(hours=requested_duration)
+                            break
+                if allocated_non_engg_requests and allocated_non_engg_requests[-1]['date'] == date and allocated_non_engg_requests[-1]['block_section_yard'] == request['block_section_yard']:
+                    break
+
+    return allocated_non_engg_requests
+
+@app.route('/optimize', methods=['GET'])
+def optimize_requests():
+    all_requests = fetch_requests_from_db()
+
+    engg_requests = [req for req in all_requests if req['dept'] == 'ENGG']
+    non_engg_requests = [req for req in all_requests if req['dept'] != 'ENGG']
+
+    # Create a hierarchy of dates based on the number of ENGG requests
+    date_hierarchy = {}
+    for req in engg_requests:
+        date = req['date']
+        if date not in date_hierarchy:
+            date_hierarchy[date] = 0
+        date_hierarchy[date] += 1
+
+    # Sort dates by the number of ENGG requests (descending)
+    sorted_dates = sorted(date_hierarchy.keys(), key=lambda x: -date_hierarchy[x])
+
+    # Allocate ENGG requests according to the hierarchy
+    allocated_requests = []
+    for date in sorted_dates:
+        allocated_requests.extend([req for req in engg_requests if req['date'] == date])
+
+    # Calculate shadow blocks based on allocated ENGG requests
+    shadow_blocks = find_shadow_blocks(allocated_requests)
+
+    # Allocate Non-ENGG requests within the shadow blocks
+    allocated_non_engg_requests = allocate_non_engg_requests(non_engg_requests, shadow_blocks, sorted_dates)
+
+    # Combine all allocated requests
+    all_allocated_requests = allocated_requests + allocated_non_engg_requests
+
+    # Clear the existing optimized table and add the optimized requests
+    db.session.query(OptimizedTable).delete()
+    for req in all_allocated_requests:
+        optimized_entry = OptimizedTable(
+            date=req['date'],
+            dept=req['dept'],
+            block_section_yard=req['block_section_yard'],
+            line='0',
+            demanded_time_from=req['start_time'],
+            demanded_time_to=req['end_time'],
+            block_demanded_in_hrs=(parse_time(req['end_time']) - parse_time(req['start_time'])).seconds / 3600,
+            location_from='0',
+            location_to='0',
+            nature_of_work='0',
+            resources_needed='0',
+            supervisors_deputed='0'
+        )
+        db.session.add(optimized_entry)
+    db.session.commit()
+    
+    return jsonify({'message': 'Optimized table created successfully'}), 200
+
+from datetime import datetime
+
+# Parse the time to convert from HH:MM:SS to HH:MM
+def convert_time_format(time_str):
+    try:
+        return datetime.strptime(time_str, "%H:%M:%S").strftime("%H:%M")
+    except ValueError:
+        return time_str  # If the time is already in HH:MM format or invalid
+
+def parse_time(time_str):
+    return datetime.strptime(time_str, "%H:%M")
+
+def is_overlapping(time1_start, time1_end, time2_start, time2_end):
+    return max(time1_start, time2_start) < min(time1_end, time2_end)
+
+def find_new_shadow_blocks(allocated_requests):
+    # Define the station sequence
+    stations = [
+        "AJJ-AJJN", "MLPM-AJJN", "AJJN-TRT", "TRT-POI", "POI-VKZ", "VKZ-NG",
+        "NG-EKM", "EKM-VGA", "VGA-PUT", "PUT-TDK", "TDK-PUDI", "PUDI-RU"
+    ]
+    
+    # Initialize shadow blocks dictionary
+    shadow_blocks = {}
+
+    # Group requests by date
+    requests_by_date = {}
+    for req in allocated_requests:
+        if req['date'] not in requests_by_date:
+            requests_by_date[req['date']] = []
+        requests_by_date[req['date']].append(req)
+
+    # Calculate shadow blocks for each date
+    for date, requests in requests_by_date.items():
+        shadow_blocks[date] = {}
+
+        # For each station, initialize the shadow block as the entire day
+        for station in stations:
+            shadow_blocks[date][station] = [{'start': parse_time('00:00'), 'end': parse_time('23:59')}]
+
+        # Adjust shadow blocks based on allocated requests
+        for req in requests:
+            start_time = parse_time(req['demanded_time_from'])
+            end_time = parse_time(req['demanded_time_to'])
+            station_index = stations.index(req['block_section_yard'])
+
+            # Update shadow blocks for all stations except the one with the request
+            for i in range(len(stations)):
+                if i == station_index:
+                    continue
+                
+                # Adjust the shadow blocks by excluding the time of the allocated request
+                updated_blocks = []
+                for block in shadow_blocks[date][stations[i]]:
+                    if is_overlapping(block['start'], block['end'], start_time, end_time):
+                        if block['start'] < start_time:
+                            updated_blocks.append({'start': block['start'], 'end': start_time})
+                        if block['end'] > end_time:
+                            updated_blocks.append({'start': end_time, 'end': block['end']})
+                    else:
+                        updated_blocks.append(block)
+                shadow_blocks[date][stations[i]] = updated_blocks
+
+    return shadow_blocks
+
+def find_shadow_blocks_for_new_request(optimized_requests):
+    shadow_blocks = find_new_shadow_blocks(optimized_requests)
+    return shadow_blocks
+
+def optimize_new_request(new_request, optimized_requests):
+    new_request_start = parse_time(new_request['demanded_time_from'])
+    new_request_end = parse_time(new_request['demanded_time_to'])
+    new_request_duration = (new_request_end - new_request_start).seconds / 3600
+
+    # Check if the requested slot is available as is
+    conflicts = [req for req in optimized_requests if req['date'] == new_request['date'] and req['block_section_yard'] == new_request['block_section_yard']]
+    
+    slot_available = True
+    for conflict in conflicts:
+        conflict_start = parse_time(conflict['demanded_time_from'])
+        conflict_end = parse_time(conflict['demanded_time_to'])
+        if is_overlapping(new_request_start, new_request_end, conflict_start, conflict_end):
+            slot_available = False
+            break
+    
+    if slot_available:
+        return new_request  # No conflict, return the original request
+
+    # If not available, find a suitable shadow block
+    shadow_blocks = find_shadow_blocks_for_new_request(optimized_requests)
+    for date, stations in shadow_blocks.items():
+        for station, blocks in stations.items():
+            if station == new_request['block_section_yard']:
+                for block in blocks:
+                    block_duration = (block['end'] - block['start']).seconds / 3600
+                    if block_duration >= new_request_duration:
+                        # Allocate this shadow block to the new request
+                        new_request['date'] = date
+                        new_request['demanded_time_from'] = block['start'].strftime("%H:%M")
+                        new_request['demanded_time_to'] = (block['start'] + timedelta(hours=new_request_duration)).strftime("%H:%M")
+                        return new_request
+    
+    # If no shadow block is available, move to the next day with no conflicts
+    next_available_date = sorted(list(set([req['date'] for req in optimized_requests])))[-1]
+    new_request['date'] = (datetime.strptime(next_available_date, '%m/%d/%Y') + timedelta(days=1)).strftime('%m/%d/%Y')
+    new_request['demanded_time_from'] = "00:00"
+    new_request['demanded_time_to'] = (parse_time("00:00") + timedelta(hours=new_request_duration)).strftime("%H:%M")
+    return new_request
+
+@app.route('/add_request', methods=['POST'])
+def add_request():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    try:
+        new_request = {
+            'date': data.get('date', '0'),
+            'dept': data.get('dept', '0'),
+            'block_section_yard': data.get('blocksectionyard', '0'),
+            'line': data.get('line', '0'),
+            'demanded_time_from': convert_time_format(data.get('demandfrom', '0')),
+            'demanded_time_to': convert_time_format(data.get('demandto', '0')),
+            'block_demanded_in_hrs': float(data.get('duration', '0').replace('hr', '').strip()) if 'duration' in data else 0,
+            'location_from': '0',
+            'location_to': '0',
+            'nature_of_work': '0',
+            'resources_needed': '0',
+            'supervisors_deputed': '0'
+        }
+
+        # Optimize the new request with the existing ones
+        optimized_requests = [{
             'date': req.date,
             'dept': req.dept,
             'block_section_yard': req.block_section_yard,
@@ -213,101 +480,30 @@ def optimize_table():
             'nature_of_work': req.nature_of_work,
             'resources_needed': req.resources_needed,
             'supervisors_deputed': req.supervisors_deputed
-        }
-        request_list.append(request_data)
+        } for req in OptimizedTable.query.all()]
 
-    optimized_requests = optimize_requests(request_list)
+        optimized_request = optimize_new_request(new_request, optimized_requests)
 
-    # Clear the existing optimized table and add the optimized requests
-    db.session.query(OptimizedTable).delete()
-    for req in optimized_requests:
+        # Save the optimized request
         optimized_entry = OptimizedTable(
-            date=req['date'],
-            dept=req['dept'],
-            block_section_yard=req['block_section_yard'],
-            line=req['line'],
-            demanded_time_from=req['demanded_time_from'],
-            demanded_time_to=req['demanded_time_to'],
-            block_demanded_in_hrs=req['block_demanded_in_hrs'],
-            location_from=req['location_from'],
-            location_to=req['location_to'],
-            nature_of_work=req['nature_of_work'],
-            resources_needed=req['resources_needed'],
-            supervisors_deputed=req['supervisors_deputed']
+            date=optimized_request['date'],
+            dept=optimized_request['dept'],
+            block_section_yard=optimized_request['block_section_yard'],
+            line=optimized_request['line'],
+            demanded_time_from=optimized_request['demanded_time_from'],
+            demanded_time_to=optimized_request['demanded_time_to'],
+            block_demanded_in_hrs=optimized_request['block_demanded_in_hrs'],
+            location_from=optimized_request['location_from'],
+            location_to=optimized_request['location_to'],
+            nature_of_work=optimized_request['nature_of_work'],
+            resources_needed=optimized_request['resources_needed'],
+            supervisors_deputed=optimized_request['supervisors_deputed']
         )
         db.session.add(optimized_entry)
-    db.session.commit()
-    
-    return jsonify({'message': 'Optimized table created successfully'}), 200
-
-@app.route('/add_request', methods=['POST'])
-def add_request():
-    data = request.get_json()
-    if not data:
-        return jsonify({'error': 'No data provided'}), 400
-
-    try:
-        new_request = RequestTable(
-            date=data.get('date', '0'),
-            dept=data.get('dept', '0'),
-            block_section_yard=data.get('blocksectionyard', '0'),
-            line=data.get('line', '0'),
-            demanded_time_from=data.get('demandfrom', '0'),
-            demanded_time_to=data.get('demandto', '0'),
-            block_demanded_in_hrs=float(data.get('duration', '0').replace('hr', '').strip()) if 'duration' in data else 0,
-            location_from='0',
-            location_to='0',
-            nature_of_work='0',
-            resources_needed='0',
-            supervisors_deputed='0'
-        )
-        db.session.add(new_request)
         db.session.commit()
 
-        # Optimize the new request with the existing ones
-        requests = RequestTable.query.all()
-        request_list = []
-        for req in requests:
-            request_data = {
-                'id': req.id,
-                'date': req.date,
-                'dept': req.dept,
-                'block_section_yard': req.block_section_yard,
-                'line': req.line,
-                'demanded_time_from': req.demanded_time_from,
-                'demanded_time_to': req.demanded_time_to,
-                'block_demanded_in_hrs': req.block_demanded_in_hrs,
-                'location_from': req.location_from,
-                'location_to': req.location_to,
-                'nature_of_work': req.nature_of_work,
-                'resources_needed': req.resources_needed,
-                'supervisors_deputed': req.supervisors_deputed
-            }
-            request_list.append(request_data)
-
-        optimized_requests = optimize_requests(request_list)
-
-        # Find the optimized allocation for the new request
-        original_request = {
-            'date': data.get('date', '0'),
-            'dept': data.get('dept', '0'),
-            'block_section_yard': data.get('blocksectionyard', '0'),
-            'line': data.get('line', '0'),
-            'demanded_time_from': data.get('demandfrom', '0'),
-            'demanded_time_to': data.get('demandto', '0'),
-            'block_demanded_in_hrs': float(data.get('duration', '0').replace('hr', '').strip()) if 'duration' in data else 0
-        }
-
-        optimized_request = None
-        for req in optimized_requests:
-            if req['dept'] == original_request['dept'] and \
-               req['block_section_yard'] == original_request['block_section_yard'] and \
-               req['block_demanded_in_hrs'] == original_request['block_demanded_in_hrs']:
-                optimized_request = req
-                break
-
         response = {
-            'original_request': original_request,
+            'original_request': new_request,
             'optimized_request': {
                 'date': optimized_request['date'],
                 'block_section_yard': optimized_request['block_section_yard'],
@@ -321,12 +517,6 @@ def add_request():
         return jsonify({'error': f'Missing field {e.args[0]}'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-def optimize_requests(requests):
-    # Placeholder for the optimization logic
-    # Sort by requested date and block section yard
-    requests.sort(key=lambda x: (x['date'], x['block_section_yard'], parse_time(x['demanded_time_from'])))
-    return requests
 
 def process_optimized_csv(filepath):
     with open(filepath, newline='') as csvfile:
